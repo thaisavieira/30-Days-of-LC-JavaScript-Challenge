0f44586c27db45693e26f821b794bf8c1a6bf5f0
Thaísa Vieira <thaisavieira.tech@proton.me>
2023-05-08 21:05:12 -0300

    Closures and arrays

---
+++ counter.js
@@ -0,0 +1,16 @@
+/*
+function init() {
+    var name = "Mozilla"; // name é uma variável local criada pelo init
+    function displayName() {
+      // displayName() é a função interna, uma closure
+      console.log(name); // usa a variável declarada na função pai
+    }
+    displayName();
+  }
+  init();
+  */
+
+  let i;
+  for (i = 0; i<=2; i++){
+    console.log(i);
+  }
